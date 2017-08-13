fb46b6ce191f37ccf61a0ab2459fdd935127ac55
Maximf <maximf@web100.com.ua>
2017-08-13 18:12:11 +0300

    autovalidation ruined headers menu?

--- src/components/views/bets/parser/RawPastedTable.vue
+++ src/components/views/bets/parser/RawPastedTable.vue
@@ -49,21 +49,18 @@ function getMainMenuItems (params) {
   return menuItems
 }
 
-var gridOptions = {
-  getMainMenuItems: getMainMenuItems
-}
-
 export default {
   name: 'RawPastedTable',
   props: ['rowData'],
   data () {
     return {
-      gridOptions: gridOptions,
+      gridOptions: {
+        getMainMenuItems: getMainMenuItems
+      },
       columnDefs: [],
       elements: [],
       resultedBets: [],
-      active: false,
-      longestElement: null
+      active: false
     }
   },
   components: {
@@ -71,40 +68,15 @@ export default {
   },
   mounted: function () {
     console.log('mounted')
+    this.loadData()
     this.rowDataChanged()
-
-    if (this.longestElement !== null) {
-      // lets try to auto recognize fields
-      console.log(this.rowData)
-      let betExample = new UIBet()
-      let _this = this
-      Object.keys(this.longestElement).forEach(function (key) {
-        Object.keys(betExample).forEach(function (kkey) {
-          // TODO
-          // validate every field for every possible value and set it (if not yet set)
-          let validated = betExample.validate(kkey, _this.longestElement[key].value, true)
-          if (validated !== false) {
-            // currentData[i][key] = validated
-            // currentData[i]['' + this.row + ''] = betExample.validate(key, value)
-            console.log('VALIDATED' + kkey)
-            console.log(_this.longestElement[key])
-            console.log(validated)
-          }
-        })
-      })
-    }
   },
   methods: {
-    rowDataChanged () {
-      console.log('rowdaachanged')
+    loadData () {
       let rows = this.rowData
       let longestElement = {}
-      let parsedBets = []
       Object.keys(rows).forEach(function (key) {
         let rawBetData = rows[key]
-        let newBet = new UIBet()
-        newBet.fill(rawBetData)
-        parsedBets.push(newBet)
 
         if (Object.keys(longestElement).length < Object.keys(rawBetData).length) {
           longestElement = rawBetData
@@ -112,23 +84,45 @@ export default {
       })
 
       // lets first the longest, which supposed to be the one with max items
+      //  also lets try to auto recognize fields
+      let betExample = new UIBet()
       var baseHeaders = []
       Object.keys(longestElement).forEach(function (key) {
-        baseHeaders.push({
-            headerName: 'select',
-            field: key,
-            cellRendererFramework: ParserCellComponent,
-            menuTabs: ['generalMenuTab']
+        let header = {
+          headerName: 'select',
+          field: key,
+          cellRendererFramework: ParserCellComponent,
+          menuTabs: ['']
+        }
+
+        Object.keys(betExample).forEach(function (kkey) {
+          // TODO
+          // validate every field for every possible value and set it (if not yet set)
+          let validated = betExample.validate(kkey, longestElement[key].value, true)
+          if (validated !== false) {
+            header['autovalidate'] = kkey
+          }
         })
+
+        baseHeaders.push(header)
       })
 
-      this.columnDefs = baseHeaders
       if (Object.keys(longestElement).length > 0) {
+        this.columnDefs = baseHeaders
         this.active = true
       }
+    },
+    rowDataChanged () {
+      console.log('rowdaachanged')
+      let rows = this.rowData
+      let parsedBets = []
+      Object.keys(rows).forEach(function (key) {
+        let rawBetData = rows[key]
+        let newBet = new UIBet()
+        newBet.fill(rawBetData)
+        parsedBets.push(newBet)
+      })
 
-      this.longestElement = longestElement
-      console.log(parsedBets)
       this.$emit('parsed', parsedBets)
     }
   }
